3326d1d283d176418ec951c5fbfd0e5fac0258eb
ft5000 <fabian.tjernstrom@gmail.com>
2024-11-23 15:01:29 +0100

    base item class

---
+++ src/models/Item.ts
@@ -0,0 +1,55 @@
+import { Attribute } from "../enums/Attribute";
+import { GameRunner } from "../GameRunner";
+import { Hero } from "./Hero";
+
+export class Item {
+    id: string;
+    name: string;
+    amount: number;
+    description: string;
+    attribute: Attribute;
+
+    constructor(id: string, name: string, description: string, attribute: Attribute) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.attribute = attribute;
+    }
+
+    public use(item: Item, hero: Hero): void {
+        switch (item.attribute) {
+            case Attribute.Healing: {
+                this.heal(hero);
+                break;
+            }
+            case Attribute.Cure: {
+                
+                break;
+            }
+        }
+    }
+
+    public heal(hero: Hero): void {
+        hero.hp += this.amount;
+    }
+
+    public healAll(): void {
+        GameRunner.get().party.forEach((hero: Hero) => {
+        this.heal(hero);
+        });
+    }
+
+    public cure(hero: Hero): void {
+        for (let effect of hero.statusEffects) {
+            if (!effect.isBuff) {
+                hero.statusEffects.splice(hero.statusEffects.indexOf(effect), 1);
+            }
+        }
+    }
+
+    public cureAll(): void {
+        GameRunner.get().party.forEach((hero: Hero) => {
+            this.cure(hero);
+        });
+    }
+}
