80ebebc996c59fb3063b31ab659d933ae0b761bb
ft5000 <fabian.tjernstrom@gmail.com>
2024-11-24 21:57:38 +0100

    change version

--- dest/enums/AppInfo.js
+++ dest/enums/AppInfo.js
@@ -1,6 +1,6 @@
 export var AppInfo;
 (function (AppInfo) {
-    AppInfo["version"] = "1.1";
+    AppInfo["version"] = "1.2";
     AppInfo[AppInfo["startingLevel"] = 1] = "startingLevel";
     AppInfo[AppInfo["numOfLevels"] = 5] = "numOfLevels";
     AppInfo[AppInfo["skipIntro"] = 0] = "skipIntro";
