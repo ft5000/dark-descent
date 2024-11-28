cbd807fbf5ad399a8376d3dfb88f216013e7c40b
ft5000 <fabian.tjernstrom@gmail.com>
2024-11-28 17:20:16 +0100

    set version

--- dest/enums/AppInfo.js
+++ dest/enums/AppInfo.js
@@ -1,8 +1,8 @@
 export var AppInfo;
 (function (AppInfo) {
-    AppInfo["version"] = "1.2";
+    AppInfo["version"] = "1.3";
     AppInfo[AppInfo["startingLevel"] = 1] = "startingLevel";
     AppInfo[AppInfo["numOfLevels"] = 5] = "numOfLevels";
     AppInfo[AppInfo["skipIntro"] = 0] = "skipIntro";
-    AppInfo[AppInfo["noDelay"] = 1] = "noDelay";
+    AppInfo[AppInfo["noDelay"] = 0] = "noDelay";
 })(AppInfo || (AppInfo = {}));
--- src/enums/AppInfo.ts
+++ src/enums/AppInfo.ts
@@ -1,7 +1,7 @@
 export enum AppInfo {
-    version = "1.2",
+    version = "1.3",
     startingLevel = 1,
     numOfLevels = 5,
     skipIntro = 0,
-    noDelay = 1,
+    noDelay = 0,
 }
