145d80eceb13afb99f1c9c8faf82f2958b598c40
ft5000 <fabian.tjernstrom@gmail.com>
2024-12-01 13:50:04 +0100

    rmdir

--- dest/enums/Attribute.js
+++
@@ -1,7 +0,0 @@
-export var Attribute;
-(function (Attribute) {
-    Attribute["Heal"] = "Heal";
-    Attribute["HealAll"] = "HealAll";
-    Attribute["Cure"] = "Cure";
-    Attribute["CureAll"] = "CureAll";
-})(Attribute || (Attribute = {}));
--- dest/models/Enemy.js
+++
@@ -1,8 +0,0 @@
-import { Character } from "./Character.js";
-export class Enemy extends Character {
-    isEnemy = true;
-    id = null;
-    setId(id) {
-        this.id = id;
-    }
-}
